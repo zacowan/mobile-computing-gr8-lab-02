9f4bcd5ab3689f2110d847bff363627b67202449
Shaun Fidler <shaunfidler@ufl.edu>
2022-02-06 19:53:39 -0500

    Add new ResultDisplayActivity call on book click. Add back button functionality. Currently consoles item_id.

---
+++ APISearchAndroid/app/src/main/java/com/mobile_computing/ResultDisplayActivity.java
@@ -0,0 +1,13 @@
+package com.mobile_computing;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ResultDisplayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result_display);
+    }
+}
